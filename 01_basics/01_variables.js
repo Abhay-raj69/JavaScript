45a26402d15587eeabc2dcd8d7f2740e8d5cc188
Abhay Raj <93567913+Abhay-raj69@users.noreply.github.com>
2023-12-21 20:33:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=152436
+let accountEmail="bberlin609@gmail.com"
+var accountPassword="133654"
+accountCity="bhubaneswar"
+let accountState;
+
+//accountId=5622 not allowed
+
+accountEmail="rajajilelo@gmail.com"
+accountPassword="5274"
+accountCity="Patna"
+
+
+/*
+Prefer not to use var because issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//console.log(accountEmail)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3)  we are using node js, not browser
+let name="Abhay"
+let age=15
+let isLoggedin=false
+let married="no"
+
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=>standalone value
+//undefined
+//symbol=> unique
+
+
+// Object 
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
