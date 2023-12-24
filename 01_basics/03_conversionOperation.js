b2d65fe12ea30debeef60daad3a86f5e94c5fd5f
Abhay Raj <93567913+Abhay-raj69@users.noreply.github.com>
2023-12-24 19:36:38 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,43 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNumber=45
 
 let newStringNumber=String(someNumber)
-console.log(newStringNumber);
-console.log(typeof(newStringNumber));
+//console.log(newStringNumber);
+// console.log(typeof(newStringNumber));
+
+
+//*****************************Operations********************//
+
+let value=3
+
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1="Rohit"
+let str2="Sharma"
+
+let str3=str1+" "+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
