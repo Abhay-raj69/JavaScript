ba9ae265b02620a933e991fba62d26e33c21ca9a
Abhay Raj <93567913+Abhay-raj69@users.noreply.github.com>
2023-12-24 19:13:01 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score="33abs"
+
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "Rohit" => true
+// "" => false
+
+let someNumber=45
+
+let newStringNumber=String(someNumber)
+console.log(newStringNumber);
+console.log(typeof(newStringNumber));
