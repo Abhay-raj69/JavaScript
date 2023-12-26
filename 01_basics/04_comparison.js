ed3ec31ade645564d727da134f6f1eb32c2c1a5c
Abhay Raj <93567913+Abhay-raj69@users.noreply.github.com>
2023-12-26 06:58:43 +0000

    Comparison_basics

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+
+// This type of conversion can put you in confusion any time
+// So most of the time we try to avoid this type of conversion
+
+// JavaScript automatically converts that string into number
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+
+// ===
+console.log("2"==="2");
