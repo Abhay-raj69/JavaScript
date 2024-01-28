0015ff682297184d74f6f94a57a4fbecf2de1c18
Abhay Raj <93567913+Abhay-raj69@users.noreply.github.com>
2024-01-28 15:32:39 +0000

    functions final

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,4 +34,37 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+// ... this three dot is called rest operator
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,500,600));
+
+const user={
+    name:"Rohit",
+    price:200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    name:"Rohit",
+    price:499
+})
+
+const newArr=[500,600,400]
+
+
+function secondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(secondValue(newArr));
+
+console.log(secondValue([200,500,600]));
