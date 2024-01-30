de22a7f0c83de8b5d1eb926f706c66989ec05881
Abhay Raj <93567913+Abhay-raj69@users.noreply.github.com>
2024-01-30 06:39:05 +0000

    arrow function completed

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -6,10 +6,63 @@ const user={
     welcomeMessage: function(){
         // this refers to current context
         console.log(`${this.username} ,welcome to website`);
+        console.log(this);
     }
 }
 
-user.welcomeMessage()
+// user.welcomeMessage()
 
-user.username="Sharma"
-user.welcomeMessage()
+// user.username="Sharma"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username="Rohit"
+
+    // this can be only used inside an object not inside any functiohn
+//     console.log(this.username);
+// }
+
+// chai()
+
+// Same as previous 
+
+// const chai=function(){
+//     let username="Rohit"
+//     console.log(this.username);
+
+// }
+
+// chai()
+
+// This is arrow function
+
+const chai = () => {
+    let username="Rohit"
+    // console.log(this.username);
+    console.log(this);
+}
+
+// chai()
+
+
+// Explicit return
+
+// const addTwo=(num1,num2)=>{
+//     return num1+num2
+// }
+
+// implicit return 
+
+// const addTwo=(num1,num2)=> num1+num2
+
+// const addTwo=(num1,num2)=> (num1+num2)
+
+// returning object
+
+const addTwo=(num1,num2)=> ({username:"Rohit"})
+
+console.log(addTwo(10,7));
+
+
